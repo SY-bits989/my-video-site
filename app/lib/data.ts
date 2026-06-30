eff698d0a80f7ea561c1cca3bbfab2ff04a5fc44
Sy Bits <shanyuan2026@outlook.com>
2026-06-30 09:33:25 -0700

    更新

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,55 +11,56 @@ export type VideoItem = {
 export const videoData = {
   'top-video': [
     {
-      title: '神韻作品 —— 神韻早期節目: 梁山伯與祝英台片段',
+      title: '「神韻作品」 —— 神韻早期節目: 梁山伯與祝英台片段',
       url: 'https://www.shenyuncreations.com/s/t/1NOzB4',
       desc: '',
     },
     {
-      title: '神韻作品 —— 神韻早期節目: 餐館裡的笑聲',
+      title: '「神韻作品」 —— 神韻早期節目: 餐館裡的笑聲',
       url: 'https://www.shenyuncreations.com/s/t/1SQrcN',
       desc: '',
     },
     {
-      title: '神韻作品 —— 神韻早期節目: 天仙彩虹',
+      title: '「神韻作品」 —— 神韻早期節目: 天仙彩虹',
       url: 'https://www.shenyuncreations.com/s/t/1Z3VtB',
       desc: '',
     },
     {
-      title: '神韻作品 —— 神韻早期節目:擒鰲拜',
+      title: '「神韻作品」 —— 神韻早期節目:擒鰲拜',
       url: 'https://www.shenyuncreations.com/s/t/Dv8Vh',
       desc: '',
     },
     {
-      title: 'GJW+ —— 堅不可摧：神韻幕後的故事',
+      title: '「GJW+」 —— 堅不可摧：神韻幕後的故事',
       url: 'https://www.ganjingworld.com/s/K8RaWJOEl4',
       desc: '',
     },
     {
-      title: '新唐人精選新聞 ——《堅不可摧》加拿大首映座談會 神韻藝術家們暢敘',
+      title:
+        '「新唐人精選新聞」 ——《堅不可摧》加拿大首映座談會 神韻藝術家們暢敘',
       url: 'https://www.ganjingworld.com/s/E3WN3bBvVb',
       desc: '',
     },
     {
       title:
-        '新唐人電視台NTDTV —— 現場：神韻《堅不可摧：神韻幕後的故事》加拿大首映式Q&A',
+        '「新唐人電視台NTDTV」 —— 現場：神韻《堅不可摧：神韻幕後的故事》加拿大首映式Q&A',
       url: 'https://www.ganjingworld.com/s/7vVxzxYeXV',
       desc: '',
     },
     {
-      title: '新唐人精選新聞 —— 國會議員讚神韻指路明燈 頒褒獎賀重返多倫多',
+      title: '「新唐人精選新聞」 —— 國會議員讚神韻指路明燈 頒褒獎賀重返多倫多',
       url: 'https://www.ganjingworld.com/s/8MKq4kw4qQ',
       desc: '',
     },
     {
       title:
-        '加拿大新唐人中文新聞 —— 國際人權律師讚：神韻兼具藝術美感與人權內涵',
+        '「加拿大新唐人中文新聞」 —— 國際人權律師讚：神韻兼具藝術美感與人權內涵',
       url: 'https://www.ganjingworld.com/s/mwWw9lXkk3',
       desc: '',
     },
     {
       title:
-        '加拿大新唐人中文新聞 —— 國會議員褒獎 讚神韻重返多倫多是重要里程碑',
+        '「加拿大新唐人中文新聞」 —— 國會議員褒獎 讚神韻重返多倫多是重要里程碑',
       url: 'https://www.ganjingworld.com/s/7MRX3JKKRg',
       desc: '',
     },
@@ -67,22 +68,22 @@ export const videoData = {
 
   audio: [
     {
-      title: '凈泉音樂原創 —— 大法是歸度',
+      title: '「凈泉音樂原創」 —— 大法是歸度',
       url: 'https://www.joankingmusic.com/blog/music000442',
       desc: '',
     },
     {
-      title: '凈泉音樂原創 —— 萬王之王',
+      title: '「凈泉音樂原創」 —— 萬王之王',
       url: 'https://www.joankingmusic.com/blog/music000349',
       desc: '',
     },
     {
-      title: '凈泉音樂原創 —— 雨中燈',
+      title: '「凈泉音樂原創」 —— 雨中燈',
       url: 'https://www.joankingmusic.com/blog/music000394',
       desc: '',
     },
     {
-      title: '凈泉音樂原創 —— 彼岸',
+      title: '「凈泉音樂原創」 —— 彼岸',
       url: 'https://www.joankingmusic.com/blog/music000446',
       desc: '',
     },
@@ -90,38 +91,46 @@ export const videoData = {
 
   culture: [
     {
-      title: '看中國 —— 這樣做 讓你的孩子愛上閱讀',
+      title: '「看中國」 —— 這樣做 讓你的孩子愛上閱讀(圖)',
       url: 'http://kzg.io/b54cB8',
       desc: '',
     },
     {
-      title: '看中國 —— 4月份三退逾120萬 比上月增3萬 歷史在等待你的名字',
+      title:
+        '「看中國」 —— 4月份三退逾120萬 比上月增3萬 歷史在等待你的名字(組圖)',
       url: 'http://kzg.io/b54buT',
       desc: '',
     },
     {
-      title: '看中國 —— 洪水肆虐貴州廣西 山體滑坡奪命 街道變河房屋泡水',
+      title: '「看中國」 —— 洪水肆虐貴州廣西 山體滑坡奪命 街道變河房屋泡水(圖)',
       url: 'http://kzg.io/b54cQU',
       desc: '',
     },
     {
-      title: '看中國 —— 神韻20週年壓軸戲即將上演 加政要賀重返多倫多',
+      title: '「看中國」 —— 神韻20週年壓軸戲即將上演 加政要賀重返多倫多(圖)',
       url: 'http://kzg.io/b54cQc',
       desc: '',
     },
     {
-      title: '看中國 —— 神韻重返多倫多首演爆滿 觀眾：以行動反擊跨國鎮壓',
+      title:
+        '「看中國」 —— 神韻重返多倫多首演爆滿 觀眾：以行動反擊跨國鎮壓(圖)',
       url: 'http://kzg.io/b54cSz',
       desc: '',
     },
     {
-      title: '看中國 —— 香港泰拳王終圓觀神韵心願 感歎邪不勝正',
+      title: '「看中國」 —— 香港泰拳王終圓觀神韵心願 感歎邪不勝正(圖)',
       url: 'http://kzg.io/b54cU3',
       desc: '',
     },
     {
-      title: '看中國 —— 美國250週年：為何說信仰才是美國真正的立國根基？',
+      title:
+        '「看中國」 —— 美國250週年：為何說信仰才是美國真正的立國根基？(圖)',
       url: 'http://kzg.io/b54cU4',
+      desc: '',
+    },
+    {
+      title: '「看中國」 —— 願神韻恩澤天下 救華夏於危難(圖)',
+      url: 'http://kzg.io/b54cVf',
       desc: '',
     },
   ],
@@ -129,19 +138,19 @@ export const videoData = {
   others: [
     {
       title:
-        '健康1+1 —— 吃素越久，反而越容易健忘？有些人不是老了，而是大腦缺了一種關鍵營養...',
+        '「健康1+1」 —— 吃素越久，反而越容易健忘？有些人不是老了，而是大腦缺了一種關鍵營養...',
       url: 'https://www.ganjingworld.com/s/GmjgzQ8rv4',
       desc: '',
     },
     {
       title:
-        '健康1+1 —— 大便常常是深色！腸道可能出事？有些人長期大便顏色偏深，通常和飲食習慣有關。像是煎、炸、炒這類高溫料理，容易讓食物產生較多負擔性的代謝物，長期下來，可能讓腸道環境比較不理想。',
+        '「健康1+1」 —— 大便常常是深色！腸道可能出事？有些人長期大便顏色偏深，通常和飲食習慣有關。像是煎、炸、炒這類高溫料理，容易讓食物產生較多負擔性的代謝物，長期下來，可能讓腸道環境比較不理想。',
       url: 'https://www.ganjingworld.com/s/kgx0Ba8ZRo',
       desc: '',
     },
     {
       title:
-        '健康1+1 —— 按這裡！天然美白+消眼袋 如果最近：✔︎顏色暗沉 ✔︎看起來蠟黃 ✔︎眼袋越來越明顯 試試按摩 四白穴 幫助循環，讓氣色看起來更有精神。有時你需要的，不是更多保養品。而是循環變好。',
+        '「健康1+1」 —— 按這裡！天然美白+消眼袋 如果最近：✔︎顏色暗沉 ✔︎看起來蠟黃 ✔︎眼袋越來越明顯 試試按摩 四白穴 幫助循環，讓氣色看起來更有精神。有時你需要的，不是更多保養品。而是循環變好。',
       url: 'https://www.ganjingworld.com/s/NpMq0XYv2l',
       desc: '',
     },
@@ -151,7 +160,7 @@ export const videoData = {
 
   zenith: [
     {
-      title: '神韻作品 - 2023 神韻交響樂 《筷子舞》',
+      title: '「神韻作品」 - 2023 神韻交響樂 《筷子舞》',
       type: 'embed' as const,
       embedCode: `<iframe width="100%" height="500" src="https://www.shenyuncreations.com/zh-TW/embed/_video_e606bd9dcdcc48739fa9ec4a71b090c5/Mongolian-Chopsticks---2023-Shen-Yun-Symphony-Orchestra?pid=p_BkqASChRSLaH" title="2023 神韻交響樂 《筷子舞》" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;" allowfullscreen></iframe>`,
     },
@@ -161,7 +170,7 @@ export const videoData = {
   xuan: [
     {
       title:
-        '玄乎神乎 —— 150歲延壽工程還在進行;遠古國王真的活了幾千歲? 蘇美爾王表之謎',
+        '「玄乎神乎」 —— 150歲延壽工程還在進行;遠古國王真的活了幾千歲? 蘇美爾王表之謎',
       url: 'https://www.ganjingworld.com/s/6gAVMWwo4y',
       desc: '',
     },
